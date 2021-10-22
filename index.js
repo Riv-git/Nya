e3773175265f087ddefa3747052bc1ca7d88d804
Riv-git <71596903+Riv-git@users.noreply.github.com>
2021-10-22 15:44:08 -0500

    wge

--- index.js
+++ index.js
@@ -8,13 +8,7 @@ const { send } = require('process');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const Router_file = require ('./Routerfile');
-var distance = require('distance-matrix-api');
-var weather = require('openweather-apis');
-const link = "https://api.openweathermap.org/data/2.5/weather?";
-distance.key('AlphaDMAufq2jjuOxPF3pTGC4JA8fmoNzYiacAjP');
-const appid='d7b7e5b12928806a189cffd3df38513d';
-weather.setAPPID(appid);
-weather.setLang('co');
+
 
 
 
