56664f306ee10101192436866b0f901c769eb5cf
Riv-git <71596903+Riv-git@users.noreply.github.com>
2021-10-22 19:28:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express ();
 
-express()
+const bodyParser = require("body-parser");
+const { send } = require('process');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
