26d1389084032dcb6db28d93c20daebe0fc09489
Riv-git <71596903+Riv-git@users.noreply.github.com>
2021-10-22 19:19:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-app
+express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .post('/', (req,res)=>{
+  .get('/', (req, res) => {
     let tablename = req.body.name;
-    
-    res.json({ user: 'tj' });
-
-         
-})
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
