64ce4e1c1e32ef09d5868d56cc52ab031bdb6ca1
Riv-git <71596903+Riv-git@users.noreply.github.com>
2021-10-23 18:42:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,14 +14,67 @@ app
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .post('/', (req, res) => {
-    let tablename = req.body.name;
-    let possibleinstruction = 'SELECT * FROM ciudad'
-    pool.query(possibleinstruction,
-      function (error, results, fields) {
-     if (error) throw error;
-     let rowsy = JSON.parse(JSON.stringify(results[0]));
-     res.json({ user: rowsy });
-     });  
+    let ciudad = pool.escape(req.body.ciudad);
+    ciudad=ciudad.toString();
+    
+    
+
+  
+    
+
+    let fecha = pool.escape(req.body.fecha );
+    fecha=fecha.toString();
+    let int_fecha=parseInt(fecha, 10);
+
+    let valor_minimo = pool.escape(req.body.valor_minimo);
+    valor_minimo=valor_minimo.toString();
+    valor_minimo=valor_minimo.replace("'","");
+    valor_minimo=valor_minimo.replace("'","");
+    let int_valor_minimo=parseInt(valor_minimo, 10);    
+    
+
+    
+
+
+    let valor_maximo = pool.escape(req.body.valor_maximo );
+    valor_maximo=valor_maximo.toString();
+    valor_maximo=valor_maximo.replace("'","");
+    valor_maximo=valor_maximo.replace("'","");
+    let int_valor_maximo=parseInt(valor_maximo, 10);    
+    
+    
+    let tiempo_maximo = pool.escape(req.body.tiempo_maximo );
+    tiempo_maximo=tiempo_maximo.toString();
+    tiempo_maximo=tiempo_maximo.replace("'","");
+    tiempo_maximo=tiempo_maximo.replace("'","");
+    let int_tiempo_maximo=parseInt(tiempo_maximo, 10);  
+   
+
+    let tiempo_minimo = pool.escape(req.body.tiempo_minimo );
+    tiempo_minimo=tiempo_minimo.toString();
+    tiempo_minimo=tiempo_minimo.replace("'","");
+    tiempo_minimo=tiempo_minimo.replace("'","");
+    let int_tiempo_minimo=parseInt(tiempo_minimo, 10); 
+    
+
+    let actividad = pool.escape(req.body.actividad );
+    actividad=actividad.toString();
+    actividad=actividad.replace("'(", "(");
+    actividad=actividad.replace(")'", ")");
+    actividad=actividad.replace(/\\"/g, "'");
+   
+    let localizacion1= req.body.latit;
+    let localizacion2= req.body.longi;
+    
+
+    
+    
+
+
+
+    
+    let possibleinstruction = "SELECT a.nombreActividad, l.nombreLocal, a.descripcion, a.precio, a.tiempo, l.localizacion FROM actividad a left join local l on l.idlocal=a.idLocal left join ciudad c on c.idCiudad=l.idCiudad WHERE a.precio<="+int_valor_maximo+" and a.precio>="+ int_valor_minimo +" and a.tiempo<="+int_tiempo_maximo+" and a.tiempo>="+int_tiempo_minimo + " and a.tipoActividad="+actividad+" AND c.nombre = "+ ciudad + " ORDER BY a.precio desc" ;
+    res.json({ user: possibleinstruction }); 
  
 
     
