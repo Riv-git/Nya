87d3657407533937a5479ec0d8253856aa71b2c9
Riv-git <71596903+Riv-git@users.noreply.github.com>
2021-10-22 19:05:56 -0500

    weg

--- index.js
+++ index.js
@@ -1,46 +1,10 @@
-const express = require ('express');
-const app = express ();
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-app.use(express.static(__dirname + '/front'));
-var path = require('path');
-const bodyParser = require("body-parser");
-const { send } = require('process');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-var distance = require('distance-matrix-api');
-var weather = require('openweather-apis');
-const link = "https://api.openweathermap.org/data/2.5/weather?";
-distance.key('AlphaDMAufq2jjuOxPF3pTGC4JA8fmoNzYiacAjP');
-const appid='d7b7e5b12928806a189cffd3df38513d';
-weather.setAPPID(appid);
-weather.setLang('co');
-
-
-
-
-
-
-
-
-// ./ for same level archive
-
-
-app.post('/', (req,res)=>{
-   let tablename = req.body.name;
-   res.json({ user: 'example' });
-   
-   
-
-   
-
-    
-});
-
-
-
-
-const port_number = process.env.port || 3000; 
-
-app.listen(port_number, function(){
-    console.log('Possible instructions/message while listening to ' + port_number+ '');
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
